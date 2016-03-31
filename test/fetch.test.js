c39187a3413fb76a6c019fe05c62266abd4914da
Thomas Rueckstiess <thomas@rueckstiess.net>
2016-03-31 17:23:01 +1100

    revert to async 1.5.2, argument order has changed.

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -16,11 +16,17 @@ describe('index-model fetch()', function() {
     it('should connect to `localhost:27017` and get indexes', function(done) {
       MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
         assert.ifError(err);
-        fetch(db, 'mongodb.fanclub', function(err2, res) {
+        var collection = db.collection('_test_index_fetch');
+        collection.ensureIndex({loc: '2d'}, {w: 1}, function(err2) {
           assert.ifError(err2);
-          assert.equal(res[0].name, '_id_');
-          assert.ok(_.isNumber(res[0].size));
-          done();
+          fetch(db, 'test._test_index_fetch', function(err3, res) {
+            assert.ifError(err3);
+            assert.equal(res[0].name, '_id_');
+            assert.ok(_.isNumber(res[0].size));
+            assert.equal(res[1].name, 'loc_2d');
+            assert.ok(_.isNumber(res[1].size));
+            collection.drop(done);
+          });
         });
       });
     });
