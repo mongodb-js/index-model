cee8f62d02648da8df54c9a1e209c062e52874e8
Thomas Rueckstiess <thomas@rueckstiess.net>
2016-04-01 15:19:41 +1100

    added collection, fetchIndexes, usageHost

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var Model = require('./lib');
+var Model = require('./lib/model');
+var Collection = require('./lib/collection');
 var fetch = require('./lib/fetch');
 
-// var debug = require('debug')('mongodb-index-model');
-
 module.exports = Model;
+module.exports.Collection = Collection;
 module.exports.fetch = fetch;
